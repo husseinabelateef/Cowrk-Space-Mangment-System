492e9af02e87bb3b394d0c591b9a9805e566b7cc
husseinabelateef <husseinabdelateef96@gmail.com>
2022-03-12 01:01:20 +0200

    adding new Mvc Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cowrk_Space_Mangment_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
