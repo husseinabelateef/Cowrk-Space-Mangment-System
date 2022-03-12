bfd815c486640e55f61e0529e4160626c6542227
sohailaezz <sohaila>
2022-03-13 01:33:24 +0200

    Doaa adding (Admin, Room, Outgoing) Models

---
+++ Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cowrk_Space_Mangment_System.Models
+{
+    public class Admin
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+
+    }
+}
---
+++ Models/Outgoing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cowrk_Space_Mangment_System.Models
+{
+    public class Outgoing
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public DateTime Date { get; set; }
+
+        //[ForeignKey("reciption")]
+        //public int Reciption_ID { get; set; }
+        //public virtual Reciption reciption { get; set; }
+    }
+}
