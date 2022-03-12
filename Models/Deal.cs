53ec418847e3fb27e71740f0073cdd517fb46fe6
sohailaezz <sohaila>
2022-03-13 01:01:13 +0200

    3 classes(Deal ,Package , AssignPackage) added By sohila

---
+++ Models/Deal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cowrk_Space_Mangment_System.Models
+{
+    public class Deal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string PromoCode { get; set; }
+        public string ClassOffer { get; set; }
+        public string IndividualOrSharedOffer { get; set; }
+        public string CoffeeMachineOffer { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
