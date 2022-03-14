f95e3da983df923ee44792f9400e34ce20836939
FatmaElZahraa2 <fatmaelzahraaahmed98@gmail.com>
2022-03-14 20:53:34 +0200

    F Task

---
+++ Models/Receptionist.cs
@@ -0,0 +1,18 @@
+﻿namespace Cowrk_Space_Mangment_System.Models
+{
+    public class Receptionist
+    {
+        //Receptionist : (Name / Email / Phone / SSN / Username /Password / Salary / Shift)
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public double Salary { get; set; }
+
+        // Ask for Datatype ??
+        public int Shift { get; set; }
+        
+    }
+}
