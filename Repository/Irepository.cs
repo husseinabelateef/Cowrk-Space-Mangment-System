cafbb35fa2a1a55f2abf498b45b763911e4a3666
doaamahmoud <doaamahmoud048@gmail.com>
2022-03-17 15:45:27 +0200

    Adding Irepository interface

---
+++ Repository/Irepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Cowrk_Space_Mangment_System.Repository
+{
+    public interface Irepository <T>
+    {
+        List<T> GetAll();
+        int Delete(int id);
+        T GetById(int id);
+        int Insert(T item);
+        int Update(int id, T item);
+    }
+}
