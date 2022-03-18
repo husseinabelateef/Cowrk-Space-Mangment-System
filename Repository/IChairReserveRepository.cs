84b5091da888043bb2bf707ba09637a28c79fcd3
FatmaElZahraa2 <fatmaelzahraaahmed98@gmail.com>
2022-03-18 18:11:18 +0200

    (Chair Reserve / Receptionist / Incomming / Chair ) Repos are Done "Fatma"

---
+++ Repository/IChairReserveRepository.cs
@@ -0,0 +1,10 @@
+﻿using Cowrk_Space_Mangment_System.Models;
+using System.Collections.Generic;
+
+namespace Cowrk_Space_Mangment_System.Repository
+{
+    public interface IChairReserveRepository :Irepository<ChairReserve, int>
+    {
+        
+    }
+}
