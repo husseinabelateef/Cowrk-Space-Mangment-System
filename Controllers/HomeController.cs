929837ffd9db2f642c5ae6a3559502ae70fc244c
husseinabelateef <husseinabdelateef96@gmail.com>
2022-03-25 16:17:48 +0200

    Retriving 4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace Cowrk_Space_Mangment_System.Controllers
 {
     public class HomeController : Controller
-    {
+    { 
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
